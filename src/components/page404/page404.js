63d7a70481eec8b14893031296eef2f11dd6986b
DmitryDima <ya.cool-super-dima@yandex.ru>
2022-05-13 17:04:43 +0300

    ввод

--- src/components/page404/page404.js
+++ src/components/page404/page404.js
@@ -1,25 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Page404 = () => {
 
-  const container = {
 
-    background: "#d6eff8", /* Цвет фона */
-    padding: "10px", /* Поля вокруг текста */
-    border: "2px solid #0069b5", /* Параметры синей рамки */
-    outline: "2px solid #c52b1c",/* Параметры красной рамки */
-    width: "800px"
-
-  }
-  const body = {
-    background: "#002137"
-  }
   return (
-    <div style={body}>
+
       <div className="page404">
         <center>
-        <h1>Страница не известна. Начальная страница : <a href="/"></a></h1>
+        <h1>Страница не известна. Начальная страница : <Link to={`/`} className='card-url'>
+            НАЧАЛЬНАЯ Страница
+          </Link></h1>
         </center>
       </div>
-    </div>
+
   )
 }
 
